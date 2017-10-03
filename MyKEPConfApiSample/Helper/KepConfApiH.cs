ddbb11c631f4007f2f478c8c86f50ac6c21ff794
Kamil Yeşil <kml@live.ru>
2017-10-03 12:22:32 +0300

    modbus template

---
+++ MyKEPConfApiSample/Helper/KepConfApiH.cs
@@ -0,0 +1,140 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyKEPConfApiHelper
+{
+   public static class KepConfApiH
+    {
+        public static string kepConfApiUrl;
+        public static HttpBasicAuthenticator kepAuth;
+
+        public static int createCh(string name)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/");
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.POST);
+
+            request.AddHeader("cache-control", "no-cache");
+            // request.AddHeader("authorization", "Basic QWRtaW5pc3RyYXRvcjo=");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", KepTemplates.getModbusChJson(name,"5000"), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return (int)response.StatusCode;
+
+        }
+
+
+        public static int createDev(string chname, string name,string hostid,string port)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname + "/devices/");
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", KepTemplates.getModbusDevJson(name, hostid,port), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return (int)response.StatusCode;
+
+        }
+
+        public static int createTags(string chname, string devname, List<Tuple<string, string, int>> tags)
+        {
+
+
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname + "/devices/" + devname + "/tags");
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.POST);
+
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+
+         
+           var tagsJson = KepTemplates.getModbusTagJson(tags);
+            request.AddParameter("application/json", tagsJson, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return (int)response.StatusCode;
+
+        }
+
+
+      public static int deleteTag(string chname, string devname, string tname)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname + "/devices/" + devname + "/tags/" + tname);
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+
+            IRestResponse response = client.Execute(request);
+            return (int)response.StatusCode;
+
+        }
+
+        public static int deleteDev(string chname, string devname)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname + "/devices/" + devname);
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+
+            IRestResponse response = client.Execute(request);
+            return (int)response.StatusCode;
+
+        }
+
+        public static int deleteCh(string chname)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname);
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+
+            IRestResponse response = client.Execute(request);
+            return (int)response.StatusCode;
+
+        }
+
+
+        public static string getChListJson()
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/");
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+
+        }
+        public static string getDevListJson(string chname)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname + "/devices");
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+
+        }
+
+        public static string getTagListJson(string chname, string devname)
+        {
+            var client = new RestClient(kepConfApiUrl + "/config/v1/project/channels/" + chname + "/devices/" + devname + "/tags");
+            client.Authenticator = kepAuth;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+
+        }
+    }
+}
--- MyKEPConfApiSample/Program.cs
+++ MyKEPConfApiSample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using RestSharp;
 using RestSharp.Authenticators;
 using System;
+using MyKEPConfApiHelper;
+using System.Collections.Generic;
 
 namespace MyKEPConfApiSample
 {
@@ -17,9 +19,16 @@ namespace MyKEPConfApiSample
             Console.WriteLine("Devs:" + KepConfApiH.getDevListJson("13006")); Console.ReadLine();
             Console.WriteLine("Tags:" + KepConfApiH.getTagListJson("13006","1")); Console.ReadLine();
 
-            var x = KepConfApiH.createCh("mych");//results:  201:created //400:alrady exists //200:ok/deleted
-            KepConfApiH.createDev("mych", "mydev");
-            KepConfApiH.createTags("mych", "mydev", new string[] { "mytag","mytag2" });
+            var x = KepConfApiH.createCh("mych");//results:  201:created //400:err //200:ok/deleted
+
+            KepConfApiH.createDev("mych", "mydev", "<5.26.130.81>.1", "35001");
+
+            KepConfApiH.createTags("mych", "mydev", new List<Tuple<string, string, int>>() {
+                new Tuple<string, string, int>("mytag1", "404031", 5),
+                new Tuple<string, string, int>("mytag2", "404032", 5)
+            });
+
+            Console.ReadLine();
 
             KepConfApiH.deleteTag("mych", "mydev", "mytag");
             KepConfApiH.deleteDev("mych", "mydev");
